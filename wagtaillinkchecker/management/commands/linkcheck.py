21a4c16e05f58428db193b17cdfe6b1d5a4ca06d
Liam Brenner <liam.brenner@gmail.com>
2016-01-21 12:23:27 +1100

    Moved functions into seperate file, updated management script to be more efficient when sending emails

--- wagtaillinkchecker/management/commands/linkcheck.py
+++ wagtaillinkchecker/management/commands/linkcheck.py
@@ -1,12 +1,10 @@
-import requests
-from bs4 import BeautifulSoup
-
 from django.conf import settings
+from django.core import mail
 from django.core.mail import EmailMessage
 from django.core.management.base import BaseCommand
 from django.template.loader import render_to_string
 from wagtail.wagtailcore.models import PageRevision, Site
-from wagtaillinkchecker.views import Link
+from wagtaillinkchecker.scanner import broken_link_scan
 
 
 class Command(BaseCommand):
@@ -14,49 +12,7 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         site = Site.objects.filter(is_default_site=True).first()
         pages = site.root_page.get_descendants(inclusive=True).live().public()
-        to_crawl = set()
-        have_crawled = set()
-        broken_links = set()
-
-        for page in pages:
-            url = page.full_url
-            if not url:
-                continue
-            r1 = requests.get(url, verify=True)
-            if r1.status_code not in range(100, 300):
-                broken_links.add(Link(url, page, site=site, status_code=r1.status_code))
-                continue
-            have_crawled.add(url)
-            soup = BeautifulSoup(r1.content)
-            links = soup.find_all('a')
-            images = soup.find_all('img')
-
-            for link in links:
-                link_href = link.get('href')
-                if link_href and link_href != '#':
-                    if link_href.startswith('/'):
-                        link_href = site.root_url + link_href
-                    to_crawl.add(link_href)
-
-            for image in images:
-                image_src = link.get('src')
-                if image_src and image_src != '#':
-                    if image_src.startswith('/'):
-                        image_src = site.root_url + link_href
-                    to_crawl.add(image_src)
-
-            for link in to_crawl - have_crawled:
-                try:
-                    r2 = requests.get(link, verify=True)
-                except requests.exceptions.ConnectionError as e:
-                    broken_links.add(Link(link, page, error='There was an error connecting to this site.'))
-                    continue
-                except requests.exceptions.RequestException as e:
-                    broken_links.add(Link(link, page, site=site, error=type(e).__name__ + ': ' + str(e)))
-                    continue
-                if r2.status_code not in range(100, 300):
-                    broken_links.add(Link(link, page, site=site, status_code=r2.status_code))
-                have_crawled.add(link)
+        broken_links = broken_link_scan(site)
 
         messages = []
         for page in pages:
@@ -81,5 +37,7 @@ class Command(BaseCommand):
             email.content_subtype = 'html'
             messages.append(email)
 
-        for message in messages:
-            message.send()
+        connection = mail.get_connection()
+        connection.open()
+        connection.send_messages(messages)
+        connection.close()
--- wagtaillinkchecker/views.py
+++ wagtaillinkchecker/views.py
@@ -1,8 +1,3 @@
-from http import client
-
-import requests
-from bs4 import BeautifulSoup
-
 from django.shortcuts import redirect, render
 from django.utils.lru_cache import lru_cache
 from wagtail.wagtailadmin import messages
@@ -12,69 +7,13 @@ from wagtail.wagtailcore.models import Site
 
 from .forms import SitePreferencesForm
 from .models import SitePreferences
-
-
-class Link(Exception):
-
-    def __init__(self, url, page, status_code=None, error=None, site=None):
-        self.url = url
-        self.status_code = status_code
-        self.error = error
-        self.site = site
-        self.page = page
-
-    @property
-    def message(self):
-        if self.error:
-            return self.error
-        elif self.status_code in range(100, 300):
-            message = "Success"
-        elif self.status_code in range(500, 600) and self.url.startswith(self.site.root_url):
-            message = str(self.status_code) + ': ' + 'Internal server error, please notify the site administrator.'
-        else:
-            try:
-                message = str(self.status_code) + ': ' + client.responses[self.status_code] + '.'
-            except KeyError:
-                message = str(self.status_code) + ': ' + 'Unknown error.'
-        return message
-
-    def __str__(self):
-        return self.url
-
-    def __eq__(self, other):
-        if not isinstance(other, Link):
-            return NotImplemented
-        return self.url == other.url
-
-    def __hash__(self):
-        return hash(self.url)
+from .scanner import broken_link_scan
 
 
 @lru_cache()
 def get_edit_handler(model):
     panels = extract_panel_definitions_from_model_class(model, ['site'])
     return ObjectList(panels).bind_to_model(model)
-
-
-def get_url(url, page, site):
-    try:
-        response = requests.get(url, verify=True)
-    except requests.exceptions.ConnectionError as e:
-        raise Link(url, page, error='There was an error connecting to this site.')
-    except requests.exceptions.RequestException as e:
-        raise Link(url, page, site=site, error=type(e).__name__ + ': ' + str(e))
-    if response.status_code not in range(100, 300):
-        raise Link(url, page, site=site, status_code=response.status_code)
-    return response
-
-
-def clean_url(url, site):
-    if url and url != '#':
-        if url.startswith('/'):
-            url = site.root_url + url
-    else:
-        return None
-    return url
 
 
 def index(request):
@@ -104,43 +43,7 @@ def index(request):
 
 def scan(request):
     site = Site.find_for_request(request)
-    pages = site.root_page.get_descendants(inclusive=True).live().public()
-    to_crawl = set()
-    have_crawled = set()
-    broken_links = set()
-
-    for page in pages:
-        url = page.full_url
-        if not url:
-            continue
-        try:
-            r1 = get_url(url, page, site)
-        except Link as bad_link:
-            broken_links.add(bad_link)
-            continue
-        have_crawled.add(url)
-        soup = BeautifulSoup(r1.content)
-        links = soup.find_all('a')
-        images = soup.find_all('img')
-
-        for link in links:
-            link_href = link.get('href')
-            link_href = clean_url(link_href)
-            if link_href:
-                to_crawl.add(link_href)
-
-        for image in images:
-            image_src = link.get('src')
-            image_src = clean_url(image_src)
-            if image_src:
-                to_crawl.add(image_src)
-
-        for link in to_crawl - have_crawled:
-            try:
-                get_url(link, page, site)
-            except Link as bad_link:
-                broken_links.add(bad_link)
-            have_crawled.add(link)
+    broken_links = broken_link_scan(site)
 
     return render(request, 'wagtaillinkchecker/results.html', {
         'broken_links': broken_links,
