3ea2099dfbe4c9dbc5fd5a509ada5f01fae98d1c
Liam Brenner <liam.brenner@gmail.com>
2016-01-27 12:24:53 +1100

    Set site on scan preferences by default

--- wagtaillinkchecker/views.py
+++ wagtaillinkchecker/views.py
@@ -22,12 +22,12 @@ def index(request):
     site = Site.find_for_request(request)
     instance = SitePreferences.objects.filter(site=site).first()
     form = SitePreferencesForm(instance=instance)
+    form.instance.site = Site.find_for_request(request)
     EditHandler = get_edit_handler(SitePreferences)
 
     if request.method == "POST":
         instance = SitePreferences.objects.filter(site=site).first()
         form = SitePreferencesForm(request.POST, instance=instance)
-        form.instance.site = site
         if form.is_valid():
             edit_handler = EditHandler(instance=SitePreferences, form=form)
             form.save()
