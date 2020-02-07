07c912c2ca148fb4deb160e0b3828bdc8d1f6945
Seb <seb@neonjungle.studio>
2020-02-07 14:31:00 +1100

    Up version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='wagtail-linkchecker',
-    version='0.4.0',
+    version='0.5.0',
     description="A tool to assist with finding broken links on your wagtail site.",
     long_description=readme,
     author='Takeflight',
